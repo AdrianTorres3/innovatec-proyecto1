2214e29c4fd1c094b06f066403d3bf7f90a69260
AdrianTorres3 <117235764+AdrianTorres3@users.noreply.github.com>
2023-04-25 17:04:38 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,66 @@
+<?php
+   session_start();
+   
+   if(isset($_SESSION['clave'])){
+    header("location:inicioAdmin.php");
+   }
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <!-- Styles -->
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <!-- Google fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Muli:300,700&display=swap" rel="stylesheet">
+
+    <!-- Ionic icons -->
+    <link href="https://unpkg.com/ionicons@4.5.10-0/dist/css/ionicons.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <!-- <div class="container">
+       
+    </div> -->
+    <div class="container mx-auto tamDiv" style="width: 50%;">
+        <div class="container">
+            <img class="imagen mx-auto" src="assets/img/usuario1.png" alt="Usuario">
+            <div class="col-auto">
+                <form action="php/login_usuario.php" method="POST">
+                    <div class="form-floating m-2">
+                        
+                        <input name="clave" type="text" class="form-control" id="clave" aria-describedby="emailHelp"
+                            placeholder="Introduce Clave">
+                            <label class="form-label" for="exampleInputEmail1">Clave de usuario</label>
+                    </div>
+                    <div class="form-floating m-2">
+                        <input name="contrasena" type="password" class="form-control" id="contrasena" placeholder="Contraseña">
+                        <label for="exampleInputPassword1">Contraseña</label>
+                    </div>
+                    
+                    <div class="form-group form-check m-2">
+                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                        <label class="form-check-label" for="exampleCheck1">guardar credenciales</label>
+                    </div>
+                    <button id="iniciar-sesion" type="submit" class="btn btn-primary centrar mx-auto m-4">Iniciar sesión</button>
+                    
+                    <small id="emailHelp" class="form-text text-muted mx-auto">No estas registrado aún, 
+                        <a href="Registro.php">registrate aquí.</a></small> 
+                </form>
+            </div>
+        </div>
+    </div>
+    <script src="/js/script.js" ></script>
+</body>
+
+</html>
