4ad4abde254d3a676a3a1c7980a56894f8291c1b
AdrianTorres3 <117235764+AdrianTorres3@users.noreply.github.com>
2023-04-25 17:04:59 -0600

    Create conexion.php

---
+++ php/conexion.php
@@ -0,0 +1,10 @@
+<?php
+
+    $conexion= mysqli_connect("localhost", "root", "","empresa");
+    // if($conexion){
+    //     echo 'Conectado exitosamente';
+    // }else{
+    //     echo 'error en la conexion';
+    // }
+
+?>
